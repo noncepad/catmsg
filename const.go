af68de6ccf46b7f717096ca08ff0a561997b214b
Joel De Jesus <joel@noncepad.com>
2025-10-23 11:35:37 +0900

    update

---
+++ const.go
@@ -0,0 +1,30 @@
+package catmsg
+
+const (
+	STATUSKEY_INSTRUCTION  string = "bot_instruction_v1"
+	STATUSKEY_BOT_PUBKEY   string = "bot_pubkey_v1"
+	STATUSKEY_BOT_LAMPORTS string = "bot_lamports_v1"
+)
+
+// IsArrayEqual checks if two arrays are equal.
+func IsArrayEqual[T comparable](a, b []T) bool {
+	if len(a) != len(b) {
+		return false
+	}
+	for i := range a {
+		if a[i] != b[i] {
+			return false
+		}
+	}
+	return true
+}
+
+func IsBotPubkey(key []byte) bool {
+	x := []byte(STATUSKEY_BOT_PUBKEY)
+	return IsArrayEqual(x, key)
+}
+
+func IsBotLamports(key []byte) bool {
+	x := []byte(STATUSKEY_BOT_LAMPORTS)
+	return IsArrayEqual(x, key)
+}
